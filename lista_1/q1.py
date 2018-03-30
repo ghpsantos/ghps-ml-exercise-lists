0e8aed1fd9c5b3c52f46f04a5c92a175dc2b1f4a
ghpsantos <ghps@cin.ufpe.br>
2018-03-29 21:00:18 -0300

    saving progress in kFold cross validation

--- lista_1/q1.py
+++ lista_1/q1.py
@@ -6,6 +6,14 @@ Created on Wed Mar 28 18:43:34 2018
 """
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 dataset_datatrieve = pd.read_csv("dataset/datatrieve.csv")
 dataset_kc2 = pd.read_csv("dataset/kc2.csv")
+
+kf = KFold(n_splits=5,shuffle=True, random_state=32)
+
+for train, test in kf.split(dataset_datatrieve):
+    print("%s %s" % (train, test))
+#kf.split(dataset_datatrieve)
