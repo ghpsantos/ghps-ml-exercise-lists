40d9a5aaa36336c37957cba84b3dc00b48a35811
ghpsantos <ghps@cin.ufpe.br>
2018-03-30 12:46:13 -0300

    adding Standart normalization

--- lista_1/q1.py
+++ lista_1/q1.py
@@ -13,17 +13,17 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
+from sklearn import preprocessing
 
 datatrieve_data = pd.read_csv("dataset/datatrieve.csv",header=None)
 kc2_data = pd.read_csv("dataset/kc2.csv",header=None)
 
 #datatrieve dataset X and y
-X_datatrieve = datatrieve_data.iloc[:,:-1].values
+X_datatrieve = preprocessing.scale(datatrieve_data.iloc[:,:-1].values)
 y_datatrieve = datatrieve_data.iloc[:,8].values
 
 #kc2 dataset X and y
-# normalizar os dados
-X_kc2 = kc2_data.iloc[:,:-1].values
+X_kc2 = preprocessing.scale(kc2_data.iloc[:,:-1].values)
 y_kc2 = kc2_data.iloc[:,21].values
 
 
