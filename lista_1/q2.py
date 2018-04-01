f8c25debe2b6c842c2af2a714b15cbb19dae853b
ghpsantos <ghps@cin.ufpe.br>
2018-03-31 23:59:30 -0300

    VDM done

--- lista_1/q2.py
+++ lista_1/q2.py
@@ -35,23 +35,23 @@ skf = StratifiedKFold(n_splits=5)
 #def euclidianDistance(v1, v2):
 #    return math.sqrt(sum(pow((v1 - v2),2)))
 #    
-#def getNearestNeighbors(instance, dataset, K):
-#    neighbours = []
-#    for i in range(len(dataset)):
-#        neighbours.append((i, euclidianDistance(instance, dataset[i,:])))
-#        
-#    neighbours = np.array(neighbours, dtype=[('index',int),('distance',float)])   
-#    
-#    return np.sort(neighbours, order='distance')[0:K]
+def getNearestNeighbors(instance, X_train_set, K, matrix, y_train_set):
+    neighbours = []
+    for i in range(len(X_train_set)):
+        neighbours.append((i, VDM(instance, X_train_set[i,:],matrix,y_train_set)))
+        
+    neighbours = np.array(neighbours, dtype=[('index',int),('distance',float)])   
+    
+    return np.sort(neighbours, order='distance')[0:K]
 
-#def getFrequencyPredictedClass(neighbours, dataset_y):
-#    classesVotes = []
-#    for i in range(len(neighbours)):
-#        (i,d) = neighbours[i]
-#        classesVotes.append(dataset_y[i])
-#    
-#    return pd.value_counts(classesVotes).keys()[0]
-#
+def getFrequencyPredictedClass(neighbours, dataset_y):
+    classesVotes = []
+    for i in range(len(neighbours)):
+        (i,d) = neighbours[i]
+        classesVotes.append(dataset_y[i])
+    
+    return pd.value_counts(classesVotes).keys()[0]
+
 #def getWeightedPredictedClass(neighbours, dataset_y):
 #    weightedVotes = {}
 #    for i in range(len(neighbours)):
@@ -66,21 +66,36 @@ skf = StratifiedKFold(n_splits=5)
 #                 
 #    return sorted(weightedVotes.items(), key=operator.itemgetter(1), reverse=True)[0][0]
 #    
+
+def VDM(a,b, matrix, y_train_set):
+    distanceVDM = 0
+    for i in range(len(a)):
+        distanceVDM += vdm(i,a[i],b[i], matrix, y_train_set)       
+        
+    return math.sqrt(distanceVDM)
     
-def buildFrequencyMatrix(X_train_set, y_train_set):
-  
+def vdm(i,ai,bi, matrix,y_train_set):
+    classes = np.unique(y_train_set)
+    distance = 0
+#    print(ai)
+#    print(bi)
+    for c in classes:
+        distance += math.pow(( matrix[''.join([str(i),str(ai),str(c)])] - matrix[''.join([str(i),str(bi),str(c)])]),2)
+#        print( matrix[''.join([str(i),str(ai),str(c)])])
+#        print( matrix[''.join([str(i),str(bi),str(c)])])
+#        print(distance)
+#        os.system("pause")
+    
+    return distance  
+
+    
+def buildProbabilityMatrix(X_train_set, y_train_set):
     classes = np.unique(y_train_set)
     n_attributes = len(X_baloons[0,:])
     
     matrix = {}
-    print(classes)
     
     for attr_index in range(n_attributes):
-#         calcular para o atributo n-esimo atributo( Colocar em um dcionario)
-#         para cada palavra, quantas vezes ela aparece no total e quantas vezes ele aparece para cada classe
-        
-#        n_total_ocurrences = 0
-#        matrix[attr_index]
         currentColumn = X_train_set[:,attr_index]
         
         for word in np.unique(currentColumn):
@@ -89,7 +104,6 @@ def buildFrequencyMatrix(X_train_set, y_train_set):
                 fc = countElementsWithClass(word, c,currentColumn, y_train_set)
                 matrix[''.join([str(attr_index),str(word),str(c)])] = (fc/f)
 
-    print(matrix)
     return matrix
 
 def countElements(element, array):
@@ -109,10 +123,13 @@ def countElementsWithClass(element, c, array, y):
     
 ### auxiliary methods end
 for train_indexes, test_indexes in skf.split(X_baloons,y_baloons):
-     buildFrequencyMatrix(X_baloons[train_indexes], y_baloons[train_indexes])
+    matrix = buildProbabilityMatrix(X_baloons[train_indexes], y_baloons[train_indexes])
+#    print(VDM(X_baloons[train_indexes][0,:], X_baloons[train_indexes][5,:],matrix, y_baloons[train_indexes]))
 #     print("TRAIN:", X_baloons[train_indexes], "TEST:", y_baloons[test_indexes], "\n\n\n -----------------")
-
-     os.system("pause")
+#    print(getNearestNeighbors(X_baloons[test_indexes][0,:], X_baloons[train_indexes], 5, matrix, y_baloons[train_indexes]))
+    neighbours = getNearestNeighbors(X_baloons[test_indexes][2,:], X_baloons[train_indexes], 5, matrix, y_baloons[train_indexes])    
+    print(getFrequencyPredictedClass(neighbours, y_baloons[train_indexes]))
+    os.system("pause")
      
 #for train, test in kf.split(dataset_datatrieve):
 #    print("%s %s" % (train, test))
