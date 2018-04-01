62df81a44cb3f99bf4d17468c0105978d42f9861
ghpsantos <ghps@cin.ufpe.br>
2018-04-01 16:23:31 -0300

    Working on HVDM

--- lista_1/q3.py
+++ lista_1/q3.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 28 18:43:34 2018
+Created on Sun Apr  1 15:26:57 2018
 
 @author: guilherme.santos
-
-Obs: Para a base de dados kc2, eu pre-processei pelo próprio excel as classes: de 'yes' para 1 e 'no' para 0
 """
+
 #test import
 import os
 #test import
@@ -27,12 +26,12 @@ from sklearn.metrics import accuracy_score
 
 
 
-baloons_data = pd.read_csv("dataset/q2/balance-scale.csv",header=None)
+crx_data = pd.read_csv("dataset/q3/crx.csv",header=None)
 #kc2_data = pd.read_csv("dataset/kc2.csv",header=None)
 
 #datatrieve dataset X and y
-X_baloons = baloons_data.iloc[:,1:5].values
-y_baloons = baloons_data.iloc[:,0].values
+X_crx = crx_data.iloc[:,:-1].values
+y_crx = crx_data.iloc[:,15].values
 
 
 #testando
@@ -50,7 +49,7 @@ skf = StratifiedKFold(n_splits=5)
 def getNearestNeighbors(instance, X_train_set, K, matrix, y_train_set):
     neighbours = []
     for i in range(len(X_train_set)):
-        neighbours.append((i, VDM(instance, X_train_set[i,:],matrix,y_train_set)))
+        neighbours.append((i, HVDM(instance, X_train_set[i,:],matrix,y_train_set)))
         
     neighbours = np.array(neighbours, dtype=[('index',int),('distance',float)])   
     
@@ -79,11 +78,12 @@ def getFrequencyPredictedClass(neighbours, dataset_y):
 #    return sorted(weightedVotes.items(), key=operator.itemgetter(1), reverse=True)[0][0]
 #    
 
-def VDM(a,b, matrix, y_train_set):
+def HVDM(a,b, matrix, y_train_set):
     distanceVDM = 0
     for i in range(len(a)):
         distanceVDM += vdm(i,a[i],b[i], matrix, y_train_set)       
-        
+        os.system("pause")
+
     return math.sqrt(distanceVDM)
     
 def vdm(i,ai,bi, matrix,y_train_set):
@@ -103,13 +103,19 @@ def vdm(i,ai,bi, matrix,y_train_set):
     
 def buildProbabilityMatrix(X_train_set, y_train_set):
     classes = np.unique(y_train_set)
-    n_attributes = len(X_baloons[0,:])
+    n_attributes = len(X_train_set[0,:])
     
     matrix = {}
-    
+    print(X_train_set[0,:])
     for attr_index in range(n_attributes):
         currentColumn = X_train_set[:,attr_index]
-        
+
+#          se o atributo da coluna X é numerico, pule(gambiarra no replace)
+        if str(X_train_set[0,:][attr_index]).replace('.','',1).isnumeric():
+#            print(attr_index)
+            print(X_train_set[0,:][attr_index])
+            continue
+
         for word in np.unique(currentColumn):
             f = countElements(word,currentColumn)
             for c in classes:
@@ -134,14 +140,15 @@ def countElementsWithClass(element, c, array, y):
     return count
     
 ### auxiliary methods end
-for train_indexes, test_indexes in skf.split(X_baloons,y_baloons):
-    matrix = buildProbabilityMatrix(X_baloons[train_indexes], y_baloons[train_indexes])
+for train_indexes, test_indexes in skf.split(X_crx,y_crx):
+    matrix = buildProbabilityMatrix(X_crx[train_indexes], y_crx[train_indexes])
     print(matrix)
 #    print(VDM(X_baloons[train_indexes][0,:], X_baloons[train_indexes][5,:],matrix, y_baloons[train_indexes]))
 #     print("TRAIN:", X_baloons[train_indexes], "TEST:", y_baloons[test_indexes], "\n\n\n -----------------")
 #    print(getNearestNeighbors(X_baloons[test_indexes][0,:], X_baloons[train_indexes], 5, matrix, y_baloons[train_indexes]))
-    neighbours = getNearestNeighbors(X_baloons[test_indexes][2,:], X_baloons[train_indexes], 5, matrix, y_baloons[train_indexes])    
-    print(getFrequencyPredictedClass(neighbours, y_baloons[train_indexes]))
+    
+#    neighbours = getNearestNeighbors(X_crx[test_indexes][2,:], X_crx[train_indexes], 5, matrix, y_crx[train_indexes])    
+#    print(getFrequencyPredictedClass(neighbours, y_crx[train_indexes]))
     os.system("pause")
      
 #for train, test in kf.split(dataset_datatrieve):
