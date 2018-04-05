508e4609f1a072336e75d4d9f5d1a3042760e71f
Guilherme dos Santos <guilherme.santos@pe.senai.br>
2018-04-05 20:24:21 -0300

    Working on LVQ2.1

--- lista_2/q1.py
+++ lista_2/q1.py
@@ -34,7 +34,7 @@ y_kc2 = kc2_data.iloc[:,21].values
 #from sklearn.model_selection import train_test_split
 #X_train, X_test, y_train, y_test  = train_test_split(X_datatrieve,y_datatrieve, test_size = 0.2, random_state = 0)
 
-knnClassifier = KNeighborsClassifier(n_neighbors=1)
+knn1Classifier = KNeighborsClassifier(n_neighbors=1)
 #neigh.fit(X_train, y_train)
 
 #print(neigh.kneighbors([X_test[3]],n_neighbors=5))
@@ -60,27 +60,67 @@ def selectRandomProtypes(n_prototypes, X_dataset,y_dataset):
 def learningRateByEpoch(learning_rate, currentEpoch, totalEpochs):
     return learning_rate*(1.0-currentEpoch/float(totalEpochs))
     
-def lvq(X_dataset, y_dataset, n_prototypes, learning_rate, epochs_quantity):
+def lvq1(X_dataset, y_dataset, n_prototypes, learning_rate, epochs_quantity):
     [selected_prototypes_X, selected_prototypes_y] = selectRandomProtypes(n_prototypes, X_dataset, y_dataset) 
-    knnClassifier.fit(selected_prototypes_X,selected_prototypes_y)
+    knn1Classifier.fit(selected_prototypes_X,selected_prototypes_y)
     
     for epoch in range(epochs_quantity):
         for i,x in enumerate(X_dataset):
             #get nearest neighbor
-            _ , neighs = knnClassifier.kneighbors([x])            
+            _ , neighs = knn1Classifier.kneighbors([x])            
             index = neighs[0][0]
             #adjusting prototype
-            if (selected_prototypes_y[index] != y_dataset[i]):
-                selected_prototypes_X[index] += (learningRateByEpoch(learning_rate, epoch, epochs_quantity)*(x - selected_prototypes_X[index]))
-            else:
+            if (selected_prototypes_y[index] == y_dataset[i]):
                 selected_prototypes_X[index] -= (learningRateByEpoch(learning_rate, epoch, epochs_quantity)*(x - selected_prototypes_X[index]))
+            else:
+                selected_prototypes_X[index] += (learningRateByEpoch(learning_rate, epoch, epochs_quantity)*(x - selected_prototypes_X[index]))
 
                 
 
     return [selected_prototypes_X, selected_prototypes_y]
-    
-[T_X, T_y] = lvq(X_datatrieve, y_datatrieve, 8, 0.3, 20)
 
+knn2Classifier = KNeighborsClassifier(n_neighbors=2)
+
+def isInWindow(distance_1, distance_2, w):
+    s = (1-w)/(1+w)
+    d1 = distance_1/distance_2
+    d2 = distance_2/distance_1
+    
+    return min(d1,d2) > s
+    
+def lvq2 (X_dataset, y_dataset, n_prototypes, learning_rate, epochs_quantity, w):
+    [P_X,p_y] = lvq1(X_dataset, y_dataset, n_prototypes, learning_rate, epochs_quantity)
+    knn2Classifier.fit(P_X, p_y)
+    
+    for epoch in range(epochs_quantity):
+        for i,x in enumerate(X_dataset):
+            distances , neighs = knn2Classifier.kneighbors([x])
+            print(neighs)
+            index_neigh_1 = neighs[0][0]
+            index_neigh_2 = neighs[0][1]
+            distance_1 = distances[0][0]
+            distance_2 = distances[0][1]
+            # if is in window
+            if isInWindow(distance_1, distance_2, w):
+                # if there different classes
+                if (p_y[index_neigh_1] != p_y[index_neigh_2]):
+                
+#                    P_X[index] -= (learningRateByEpoch(learning_rate, epoch, epochs_quantity)*(x - selected_prototypes_X[index]))
+
+        
+
+        #caiu na janela?????
+        
+
+        print(index_neigh_1)
+        print(index_neigh_2)
+        os.system("pause")
+#    print(knn2Classifier.)
+    return []
+
+    
+[T_X, T_y] = lvq1(X_datatrieve, y_datatrieve, 8, 0.3, 20)
+lvq2(X_datatrieve, y_datatrieve, 8, 0.3, 20, 555555555555)
 
 #print(T_X)
 #print(T_y)
