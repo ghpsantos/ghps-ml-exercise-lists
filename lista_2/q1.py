ecc7893e1e4ce688b13eb29855466b540acbcaad
Guilherme dos Santos <guilherme.santos@pe.senai.br>
2018-04-05 09:31:50 -0300

    Random prototype selection

--- lista_2/q1.py
+++ lista_2/q1.py
@@ -14,13 +14,14 @@ import pandas as pd
 import math
 import operator
 import numpy as np
+import random
 from sklearn.model_selection import StratifiedKFold
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 
-datatrieve_data = pd.read_csv("dataset/q1/datatrieve.csv",header=None)
-kc2_data = pd.read_csv("dataset/q1/kc2.csv",header=None)
+datatrieve_data = pd.read_csv("dataset/datatrieve.csv",header=None)
+kc2_data = pd.read_csv("dataset/kc2.csv",header=None)
 
 #datatrieve dataset X and y
 X_datatrieve = preprocessing.scale(datatrieve_data.iloc[:,:-1].values)
@@ -30,14 +31,33 @@ y_datatrieve = datatrieve_data.iloc[:,8].values
 X_kc2 = preprocessing.scale(kc2_data.iloc[:,:-1].values)
 y_kc2 = kc2_data.iloc[:,21].values
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test  = train_test_split(X_datatrieve,y_datatrieve, test_size = 0.2, random_state = 0)
+#from sklearn.model_selection import train_test_split
+#X_train, X_test, y_train, y_test  = train_test_split(X_datatrieve,y_datatrieve, test_size = 0.2, random_state = 0)
 
 neigh = KNeighborsClassifier( n_neighbors=3)
-neigh.fit(X_train, y_train)
+#neigh.fit(X_train, y_train)
 
-print(neigh.kneighbors([X_test[3]],n_neighbors=5))
+#print(neigh.kneighbors([X_test[3]],n_neighbors=5))
 #skf = StratifiedKFold(n_splits=5)
 
 def euclidianDistance(v1, v2):
     return math.sqrt(sum(pow((v1 - v2),2)))
+
+#n_prototypes gets N of each class in the dataset
+def selectRandomProtypes(n_prototypes, X_dataset,y_dataset):
+    random_prototypes = []
+    classes = np.unique(y_dataset)
+    for c in classes:
+        selected_indexes = random.sample(np.asarray(np.where(y_datatrieve==1)).tolist()[0],n_prototypes)
+        
+        for i in selected_indexes:
+            random_prototypes.append(np.asarray(X_dataset[i]).tolist())
+    
+    return np.array(random_prototypes)
+    
+def lvq(X_dataset, y_dataset, n_prototypes, learning_rate, epochs_quantity):
+    selected_prototypes = []
+
+    
+
+    return selected_prototypes
