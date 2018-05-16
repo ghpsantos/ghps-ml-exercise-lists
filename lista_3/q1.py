2354c40d620921ab49b29ba3e2ebef61e1c62dcd
Guilherme dos Santos <guilherme.santos@pe.senai.br>
2018-05-16 18:51:43 -0300

    working on evaluation

--- lista_3/q1.py
+++ lista_3/q1.py
@@ -24,6 +24,13 @@ iris_data = pd.read_csv("dataset/iris.csv", header=None)
 X_iris = preprocessing.scale(iris_data.iloc[:,:-1].values)
 y_iris = iris_data.iloc[:,4].values
 
+
+datatrieve_data = pd.read_csv("dataset/datatrieve.csv",header=None)
+
+#datatrieve dataset X and y
+X_datatrieve = preprocessing.scale(datatrieve_data.iloc[:,:-1].values)
+y_datatrieve = datatrieve_data.iloc[:,8].values
+
 #
 #datatrieve_data = pd.read_csv("dataset/datatrieve.csv",header=None)
 #X_datatrieve = preprocessing.scale(datatrieve_data.iloc[:,:-1].values)
@@ -46,6 +53,7 @@ def sortEigenComponents(eig_vals, eig_vecs):
     
 def LDA(X,y):
     classes = np.unique(y).tolist()
+
     Sw = 0
     Sb = 0
 
@@ -73,13 +81,13 @@ def LDA(X,y):
             Sw = Sw + np.matmul(np.transpose([mean_diff]), np.array([mean_diff]));
 
 
-    eig_vals, eig_vecs = np.linalg.eig(np.dot(np.linalg.inv(Sw),Sb))
+    eig_vals, eig_vecs = np.linalg.eig(np.matmul(np.linalg.inv(Sw),Sb))
     
     eig_map = sortEigenComponents(eig_vals,eig_vecs)
     
     return eig_map  
     
-LDA(X_kc2, y_kc2)
+#LDA(X_kc2, y_kc2)
 #LDA(X_iris, y_iris)
 #LDA(X_cm1, y_cm1)
 
@@ -89,7 +97,6 @@ def buildProjectionMatrix(eig_map, n_components):
     projection_matrix = []
 
     for i in range(n_components):
-        print(np.transpose([eig_map[i][1]]))
         projection_matrix.append(np.transpose([eig_map[i][1]]))
     
     #join arrays by column
@@ -109,11 +116,10 @@ def PCA(X):
     return eig_map
 
 #components = PCA(X_iris)
-components = PCA(X_cm1)
-components_lda = LDA(X_cm1,y_cm1)
-print(len(components))
-print(len(components_lda))
-projectionMatrix = buildProjectionMatrix(components, 3)
+#components = PCA(X_cm1)
+#components_lda = LDA(X_cm1,y_cm1)
+#
+#projectionMatrix = buildProjectionMatrix(components, 3)
 
 #X_iris_projected = np.matmul(X_iris,projectionMatrix)
 
@@ -135,30 +141,74 @@ skf = StratifiedKFold(n_splits=5)
 from sklearn.neighbors import KNeighborsClassifier
 knn3 = KNeighborsClassifier(n_neighbors=3)
 
+
+def runKnnAndGetScores(X_train, y_train, X_test, y_test):
+    #train
+    knn3.fit(X_train, y_train)
+    #accuracies
+    score = knn3.score(X_test, y_test)
+    print('oi, matrix de precisão' )
+    print(score)
+#    os.system("pause")
+    return []
+
 def runStratifiedKFoldAndPlotResult(X,y):
     n_attributes = len(X[0])
+    print(n_attributes)
     for train_indexes, test_indexes in skf.split(X,y):
         #getting components from train set
         components_pca = PCA(X[train_indexes])
         components_lda = LDA(X[train_indexes],y[train_indexes])
-        #for each quantity of attribute
+        print('\n\n\n\n\n')
+        print('queeeee')
+        print(components_pca)
+        print(components_lda)
+#        os.system("pause")
+#        #for each quantity of attribute
+#        
         for c_v in range(1,n_attributes+1):
+            print(c_v)
             projectionMatrix_pca = buildProjectionMatrix(components_pca, c_v)
             projectionMatrix_lda = buildProjectionMatrix(components_lda, c_v)
             
-            X_projeced_pca = np.matmul(X_iris,projectionMatrix_pca)
-            X_projeced_lda = np.matmul(X_iris,projectionMatrix_lda)
+            print(projectionMatrix_lda)
+            print(projectionMatrix_pca)
+
             
+#            os.system("pause")
+            
+            projected_X_train_pca  = np.matmul(X[train_indexes],projectionMatrix_pca)
+            projected_X_train_lda =  np.matmul(X[train_indexes],projectionMatrix_lda)
+            
+            projected_X_test_pca  = np.matmul(X[test_indexes],projectionMatrix_pca)
+            projected_X_test_lda =  np.matmul(X[test_indexes],projectionMatrix_lda)
+
+            
+#            X_projeced_pca = np.matmul(X_iris,projectionMatrix_pca)
+#            X_projeced_lda = np.matmul(X_iris,projectionMatrix_lda)
+            
+            print('PCA')
+            runKnnAndGetScores(projected_X_train_pca, y[train_indexes], projected_X_test_pca, y[test_indexes])
+            print('LDA')
+            runKnnAndGetScores(projected_X_train_lda, y[train_indexes], projected_X_test_lda, y[test_indexes])
+
+            os.system("pause")
+#            
             #fazer aqui uma função para pegar a matrix de projeção e o X, treinar e pegar o score. depois colocar em um array e tirar a média
             
 #        print(len(X[train_indexes][0]))
-            os.system("pause")
+            
         
         
-runStratifiedKFoldAndPlotResult(X_iris,y_iris)
-    
+#runStratifiedKFoldAndPlotResult(X_iris,y_iris)
+#runStratifiedKFoldAndPlotResult(X_kc2,y_kc2)
+#runStratifiedKFoldAndPlotResult(X_datatrieve,y_datatrieve)
 
+l = range(1,12)
+print(l)
 #mockedDataset = [[1,0.5],[2,0.6],[3,0.7],[3,0.8],[4,0.9],[5,0.556], [6,0.5],[7,0.6],[8,0.7],[9,0.8],[10,0.9],[11,0.556]]
+#barPlot(mockedDataset)
+
 #df = pd.DataFrame(mockedDataset, columns=['components','precision'])
 #print(df)
 #
