56342ee780288cee462170388f17a9a8db3e9ca9
Guilherme Henrique <ghps@cin.ufpe.br>
2018-05-20 21:29:55 -0300

    Refatorando: criando um arquivo para o LDA

--- lista_3/q2.py
+++ lista_3/q2.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 14 22:10:23 2018
+Created on Sun May 20 20:55:33 2018
 
 @author: Guilherme
 """
+
 
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 import os
 
+
 cm1_data = pd.read_csv("dataset/cm1.csv", header=None)
 X_cm1 = preprocessing.scale(cm1_data.iloc[:,:-1].values)
 y_cm1 = cm1_data.iloc[:,21].values
+
 
 iris_data = pd.read_csv("dataset/iris.csv", header=None)
 X_iris = preprocessing.scale(iris_data.iloc[:,:-1].values)
@@ -30,12 +33,46 @@ def sortEigenComponents(eig_vals, eig_vecs):
     for i, eig_val in enumerate(eig_vals):
         eig_map.append((eig_val,eig_vecs[i].tolist()))
         
-
+        
     eig_map = sorted(eig_map, key=lambda my_tuple: my_tuple[0], reverse=True)
     return eig_map
     
-    
+def LDA(X,y):
+    classes = np.unique(y).tolist()
 
+    Sw = 0
+    Sb = 0
+
+    overall_mean_dataset = np.mean(X, axis=0)
+
+    for c in classes:
+        indexes = np.where(y == c)[0]
+        
+        #número de padrões da classe l
+        nl = len(indexes)
+        #class instances
+        classSamples = X[indexes]
+        #overall mean classs
+        overall_mean_class = np.mean(classSamples, axis=0)
+        
+#        print(overall_mean_class)
+        #Sb calculation
+        mean_diff = overall_mean_class - overall_mean_dataset
+        Sb = Sb + nl*(np.matmul(np.transpose([mean_diff]), np.array([mean_diff])))
+        
+        #Sw calculation
+        for i in range(nl):
+            mean_diff = classSamples[i,:] - overall_mean_class
+            
+            Sw = Sw + np.matmul(np.transpose([mean_diff]), np.array([mean_diff]));
+
+
+    eig_vals, eig_vecs = np.linalg.eig(np.matmul(np.linalg.inv(Sw),Sb))
+    
+    eig_map = sortEigenComponents(eig_vals,eig_vecs)
+    
+    return eig_map  
+    
 def buildProjectionMatrix(eig_map, n_components):
     projection_matrix = []
 
@@ -44,20 +81,7 @@ def buildProjectionMatrix(eig_map, n_components):
     
     #join arrays by column
     return np.hstack(projection_matrix)
-
-########## PCA
-def PCA(X):
-    #calculate the covariance matrix
-    cov_matrix = np.cov(X.T)
-    # calculate eigenvalues and vectors
-    eig_vals, eig_vecs = np.linalg.eig(cov_matrix)
-
-    #sort eigens
-    eig_map = sortEigenComponents(eig_vals,eig_vecs)
     
-    return eig_map
-
-
 
 #experiment and plot
 import seaborn as sns
@@ -68,7 +92,6 @@ def barPlot(data, plot_title):
     
     df = pd.DataFrame(data, columns=['components','precision'])
     sns.set_style("whitegrid")
-    plt.figure(figsize=(8,5))
     g = sns.barplot(x="components", y="precision", data=df)
     g.set_title(plot_title)
     g.set_ylim(0, 1)
@@ -89,11 +112,10 @@ def runKnnAndGetScores(X_train, y_train, X_test, y_test):
     knn3.fit(X_train, y_train)
     #accuracies
     score = knn3.score(X_test, y_test)
-#    os.system("pause")
+
     return score
 
-def buildPDFramePCA(mean_array, max_range):
-    
+def buildPDFrameLDA(mean_array, max_range):
     pd_format = []
     for i in range(1,max_range+1):
         if i != (max_range):
@@ -104,51 +126,53 @@ def buildPDFramePCA(mean_array, max_range):
     df = pd.DataFrame(pd_format, columns=['components','precision'])
     return df
 
-
-#########################
 def runStratifiedKFoldAndGetFrame(X,y):
-    n_attributes = len(X[0])    
+    n_classes = len(np.unique(y).tolist())        
 
-    precision_matrix_pca = []
+    precision_matrix_lda = []
+
 
     for train_indexes, test_indexes in skf.split(X,y):
         #getting components from train set
-        components_pca = PCA(X[train_indexes])
+        components_lda = LDA(X[train_indexes],y[train_indexes])
        
-        iteration_precision_line_pca = []
+        iteration_precision_line_lda = []
 
         #for each quantity of attribute
-        for c_v in range(1,n_attributes):
-            #build the projection matrix of the components
-            projectionMatrix_pca = buildProjectionMatrix(components_pca, c_v)
-            #projected train set
-            projected_X_train_pca  = np.matmul(X[train_indexes],projectionMatrix_pca)
-            #projected test set
-            projected_X_test_pca  = np.matmul(X[test_indexes],projectionMatrix_pca)
+
+        for c_v in range(1,n_classes):
+            print('oi')
+            projectionMatrix_lda = buildProjectionMatrix(components_lda, c_v)
+                  
+            projected_X_train_lda =  np.matmul(X[train_indexes],projectionMatrix_lda)
             
-            score_pca = runKnnAndGetScores(projected_X_train_pca, y[train_indexes], projected_X_test_pca, y[test_indexes])
+            projected_X_test_lda =  np.matmul(X[test_indexes],projectionMatrix_lda)
+
+            score_lda = runKnnAndGetScores(projected_X_train_lda, y[train_indexes], projected_X_test_lda, y[test_indexes])
             
-            iteration_precision_line_pca.append(score_pca)
+            iteration_precision_line_lda.append(score_lda)
             
-                    
-        #appending the original dataset precision
+
+         #appending the original dataset precision
         original_dataset_score = runKnnAndGetScores(X[train_indexes], y[train_indexes], X[test_indexes], y[test_indexes])
-        iteration_precision_line_pca.append(original_dataset_score)
+        iteration_precision_line_lda.append(original_dataset_score)
         #####
         
-        precision_matrix_pca.append(iteration_precision_line_pca)
-    
+        precision_matrix_lda.append(iteration_precision_line_lda)
         
-        
-    return buildPDFramePCA(np.mean(np.array(precision_matrix_pca),axis=0),n_attributes)
+
 
     
+    return buildPDFrameLDA(np.mean(np.array(precision_matrix_lda),axis=0),n_classes)   
+
 def runExperimentAndPlot(X,y, dataset_name):
     pd_frame_pca = runStratifiedKFoldAndGetFrame(X,y)
-    barPlot(pd_frame_pca, dataset_name + ' PCA')
-
-
+#    os.system("pause")
+    barPlot(pd_frame_pca, dataset_name + ' LDA')
+    
 
 #runExperimentAndPlot(X_iris,y_iris, 'IRIS')
-runExperimentAndPlot(X_cm1,y_cm1, 'CM1')
+#runExperimentAndPlot(X_cm1,y_cm1, 'CM1')
 #runExperimentAndPlot(X_datatrieve,y_datatrieve,'DATATRIEVE')
+
+     
